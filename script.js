0623f3cda2ef6ff3738c3c40de12f5a6b354c270
berkayakpinar <brky45@gmail.com>
2022-12-02 03:28:16 +0300

    Update

--- script.js
+++ script.js
@@ -4,13 +4,30 @@ const navText = document.querySelectorAll(".nav-text");
 const nav = document.querySelector(".nav");
 const overlay = document.querySelector(".overlay");
 const ctx = document.getElementById("pieChart");
+const addText = document.querySelector(".text");
+const addModal = document.querySelector(".add-modal");
+const pharmacyList = document.querySelector(".pharmacy-list");
 
-const blur = function () {
-  overlay.classList.toggle("hidden");
+const iptName = document.querySelector(".name");
+const iptAdress = document.querySelector(".adress");
+const iptCity = document.querySelector(".city");
+
+const btnAdd = document.querySelector(".add");
+const btnBack = document.querySelector(".back");
+
+const onBlur = function () {
+  overlay.classList.remove("hidden");
 };
 
-nav.addEventListener("mouseover", blur);
-nav.addEventListener("mouseout", blur);
+const onScreen = function (item) {
+  overlay.classList.add("hidden");
+  addModal.classList.add("hidden");
+};
+
+const openModal = function () {
+  addModal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+};
 
 /*CHART*/
 const data = {
@@ -35,4 +52,22 @@ const data = {
 new Chart(ctx, {
   type: "doughnut",
   data: data,
+});
+
+nav.addEventListener("mouseover", onBlur);
+nav.addEventListener("mouseout", onScreen);
+addText.addEventListener("click", openModal);
+overlay.addEventListener("click", onScreen);
+btnBack.addEventListener("click", onScreen);
+btnAdd.addEventListener("click", (e) => {
+  if (iptName || iptAdress || iptCity !== "") {
+    const newLi = document.createElement("li");
+    const newName = document.createElement("p");
+    const newAdress = document.createElement("p");
+    newName.innerHTML = iptName.value;
+    newAdress.innerHTML = iptAdress.value;
+    newLi.title = newName;
+    newLi.Adress = newAdress;
+    pharmacyList.appendChild(newLi);
+  }
 });
